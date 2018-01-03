6367508d62eef7776a47d02b05b194bdc2232c4b
Saghm Rossi <saghmrossi@gmail.com>
2018-01-03 15:50:43 -0500

    GODRIVER-159 Implement bson.Constructor

--- bson/constructor.go
+++ bson/constructor.go
@@ -81,8 +81,16 @@ func (c Constructor) ArrayFromElements(key string, elems ...*Element) *Element {
 	return c.Array(key, NewDocument(uint(len(elems))).Append(elems...))
 }
 
-func (Constructor) Binary(key string, b []byte, btype byte) *Element {
+func (c Constructor) Binary(key string, b []byte) *Element {
+	return c.BinaryWithSubtype(key, b, 0)
+}
+
+func (Constructor) BinaryWithSubtype(key string, b []byte, btype byte) *Element {
 	size := uint32(1 + len(key) + 1 + 4 + 1 + len(b))
+	if btype == 2 {
+		size += 4
+	}
+
 	buf := make([]byte, size)
 	elem := new(Element)
 	elem.start = 0
@@ -91,7 +99,8 @@ func (Constructor) Binary(key string, b []byte, btype byte) *Element {
 	if err != nil {
 		panic(err)
 	}
-	elem.data = b
+
+	elem.data = buf
 	return elem
 }
 
@@ -113,9 +122,51 @@ func (Constructor) Undefined(key string) *Element {
 	return elem
 }
 
-func (Constructor) ObjectID(key string, obj [12]byte) *Element { return nil }
-func (Constructor) Boolean(key string, b bool) *Element        { return nil }
-func (Constructor) DateTime(key string, dt int64) *Element     { return nil }
+func (Constructor) ObjectID(key string, oid [12]byte) *Element {
+	size := uint32(1 + len(key) + 1 + 12)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.ObjectId.Element(0, elem.data, key, oid)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) Boolean(key string, b bool) *Element {
+	size := uint32(1 + len(key) + 1 + 1)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Boolean.Element(0, elem.data, key, b)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) DateTime(key string, dt int64) *Element {
+	size := uint32(1 + len(key) + 1 + 8)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.DateTime.Element(0, elem.data, key, dt)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
 func (Constructor) Null(key string) *Element {
 	size := uint32(1 + len(key) + 1)
 	b := make([]byte, size)
@@ -133,17 +184,195 @@ func (Constructor) Null(key string) *Element {
 	elem.data = b
 	return elem
 }
-func (Constructor) Regex(key string, pattern, options string) *Element            { return nil }
-func (Constructor) DBPointer(key string, dbpointer [12]byte) *Element             { return nil }
-func (Constructor) Javascript(key string, js string) *Element                     { return nil }
-func (Constructor) Symbol(key string, symbol string) *Element                     { return nil }
-func (Constructor) CodeWithScope(key string, js string, scope *Document) *Element { return nil }
-func (Constructor) Int32(key string, i int32) *Element                            { return nil }
-func (Constructor) Uint64(key string, u uint64) *Element                          { return nil }
-func (Constructor) Int64(key string, i int64) *Element                            { return nil }
-func (Constructor) Decimal128(key string, d ast.Decimal128) *Element              { return nil }
-func (Constructor) MinKey(key string) *Element                                    { return nil }
-func (Constructor) MaxKey(key string) *Element                                    { return nil }
+func (Constructor) Regex(key string, pattern, options string) *Element {
+	size := uint32(1 + len(key) + 1 + len(pattern) + 1 + len(options) + 1)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Regex.Element(0, elem.data, key, pattern, options)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) DBPointer(key string, ns string, oid [12]byte) *Element {
+	size := uint32(1 + len(key) + 1 + 4 + len(ns) + 1 + 12)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.DBPointer.Element(0, elem.data, key, ns, oid)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+func (Constructor) Javascript(key string, code string) *Element {
+	size := uint32(1 + len(key) + 1 + 4 + len(code) + 1)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Javascript.Element(0, elem.data, key, code)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) Symbol(key string, symbol string) *Element {
+	size := uint32(1 + len(key) + 1 + 4 + len(symbol) + 1)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Symbol.Element(0, elem.data, key, symbol)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) CodeWithScope(key string, code string, scope *Document) *Element {
+	size := uint32(1 + len(key) + 1 + 4 + 4 + len(code) + 1)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+	elem.d = scope
+
+	_, err := elements.Byte.Encode(0, elem.data, '\x0F')
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = elements.CString.Encode(1, elem.data, key)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = elements.Int32.Encode(1+uint(len(key))+1, elem.data, int32(size))
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = elements.String.Encode(1+uint(len(key))+1+4, elem.data, code)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) Int32(key string, i int32) *Element {
+	size := uint32(1 + len(key) + 1 + 4)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Int32.Element(0, elem.data, key, i)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) Timestamp(key string, t uint32, i uint32) *Element {
+	size := uint32(1 + len(key) + 1 + 8)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Timestamp.Element(0, elem.data, key, t, i)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) Int64(key string, i int64) *Element {
+	size := uint32(1 + len(key) + 1 + 8)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Int64.Element(0, elem.data, key, i)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) Decimal128(key string, d ast.Decimal128) *Element {
+	size := uint32(1 + len(key) + 1 + 16)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Decimal128.Element(0, elem.data, key, d)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) MinKey(key string) *Element {
+	size := uint32(1 + len(key) + 1)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Byte.Encode(0, elem.data, '\xFF')
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = elements.CString.Encode(1, elem.data, key)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
+
+func (Constructor) MaxKey(key string) *Element {
+	size := uint32(1 + len(key) + 1)
+	elem := new(Element)
+	elem.data = make([]byte, size)
+	elem.start = 0
+	elem.value = uint32(1 + len(key) + 1)
+
+	_, err := elements.Byte.Encode(0, elem.data, '\x7F')
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = elements.CString.Encode(1, elem.data, key)
+	if err != nil {
+		panic(err)
+	}
+
+	return elem
+}
 
 func (ArrayConstructor) Double(f float64) *Element            { return nil }
 func (ArrayConstructor) String(val string) *Element           { return nil }
---
+++ bson/constructor_test.go
@@ -0,0 +1,425 @@
+package bson
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/skriptble/wilson/bson/parser/ast"
+	"github.com/stretchr/testify/require"
+)
+
+func requireElementsEqual(t *testing.T, expected *Element, actual *Element) {
+	require.Equal(t, expected.start, actual.start)
+	require.Equal(t, expected.value, actual.value)
+
+	require.True(t, bytes.Equal(expected.data, actual.data))
+
+	if expected.d == nil {
+		require.Nil(t, actual.d)
+	} else {
+		require.NotNil(t, actual.d)
+		require.Equal(t, expected.d.IgnoreNilInsert, actual.d.IgnoreNilInsert)
+
+		require.Equal(t, len(expected.d.elems), len(actual.d.elems))
+		for i := range expected.d.elems {
+			requireElementsEqual(t, expected.d.elems[i], actual.d.elems[i])
+		}
+
+		require.Equal(t, len(expected.d.index), len(actual.d.index))
+		for i := range expected.d.index {
+			require.Equal(t, expected.d.index[i], actual.d.index[i])
+		}
+	}
+}
+
+func TestConstructor(t *testing.T) {
+	t.Run("Double", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x1,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value
+			0x6e, 0x86, 0x1b, 0xf0, 0xf9,
+			0x21, 0x9, 0x40,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Double("foo", 3.14159)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("String", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x2,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value - string length
+			0x4, 0x0, 0x0, 0x0,
+			// value - string
+			0x62, 0x61, 0x72, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.String("foo", "bar")
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("SubDocument", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x3,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+		}
+		d := NewDocument()
+		d.Append(C.String("bar", "baz"))
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: d}
+		actual := C.SubDocument("foo", d)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("SubDocumentFromElements", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x3,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+		}
+		e := C.String("bar", "baz")
+		d := NewDocument()
+		d.Append(e)
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: d}
+		actual := C.SubDocumentFromElements("foo", e)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Array", func(t *testing.T) {
+		// TODO GODRIVER-160
+	})
+
+	t.Run("ArrayFromElements", func(t *testing.T) {
+		// TODO GODRIVER-160
+	})
+
+	t.Run("Binary", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x5,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value - binary length
+			0x7, 0x0, 0x0, 0x0,
+			// value - binary subtype
+			0x0,
+			// value - binary data
+			0x8, 0x6, 0x7, 0x5, 0x3, 0x0, 0x9,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Binary("foo", []byte{8, 6, 7, 5, 3, 0, 9})
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("BinaryWithSubtype", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x5,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value - binary length
+			0xb, 0x0, 0x0, 0x0,
+			// value - binary subtype
+			0x2,
+			//
+			0x07, 0x00, 0x00, 0x00,
+			// value - binary data
+			0x8, 0x6, 0x7, 0x5, 0x3, 0x0, 0x9,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.BinaryWithSubtype("foo", []byte{8, 6, 7, 5, 3, 0, 9}, 2)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Undefined", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x6,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Undefined("foo")
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("ObjectId", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x7,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value
+			0x5a, 0x15, 0xd0, 0xa4, 0xd5, 0xda, 0xa5, 0xf1, 0x0a, 0x5e, 0x10, 0x89,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.ObjectID(
+			"foo",
+			[12]byte{0x5a, 0x15, 0xd0, 0xa4, 0xd5, 0xda, 0xa5, 0xf1, 0x0a, 0x5e, 0x10, 0x89},
+		)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Boolean", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x8,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value
+			0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Boolean("foo", false)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("DateTime", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x9,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value
+			0x11, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.DateTime("foo", 17)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Null", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0xa,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Null("foo")
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Regex", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0xb,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value - pattern
+			0x62, 0x61, 0x72, 0x0,
+			// value - options
+			0x69, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Regex("foo", "bar", "i")
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("DBPointer", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0xc,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value - namespace length
+			0x4, 0x0, 0x0, 0x0,
+			// value - namespace
+			0x62, 0x61, 0x72, 0x0,
+			// value - oid
+			0x5a, 0x15, 0xd0, 0xa4, 0xd5, 0xda, 0xa5, 0xf1, 0x0a, 0x5e, 0x10, 0x89,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.DBPointer(
+			"foo",
+			"bar",
+			[12]byte{0x5a, 0x15, 0xd0, 0xa4, 0xd5, 0xda, 0xa5, 0xf1, 0x0a, 0x5e, 0x10, 0x89},
+		)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("JavaScriptCode", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0xd,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value - code length
+			0xd, 0x0, 0x0, 0x0,
+			// value - code
+			0x76, 0x61, 0x72, 0x20, 0x62, 0x61, 0x72, 0x20, 0x3d, 0x20, 0x33, 0x3b, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Javascript("foo", "var bar = 3;")
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Symbol", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0xe,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value - string length
+			0x4, 0x0, 0x0, 0x0,
+			// value - string
+			0x62, 0x61, 0x72, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Symbol("foo", "bar")
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("CodeWithScope", func(t *testing.T) {
+		buf := []byte{
+			0xf,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value - code length
+			0x1a, 0x0, 0x0, 0x0,
+			// value - length
+			0xd, 0x0, 0x0, 0x0,
+			// value - code
+			0x76, 0x61, 0x72, 0x20, 0x62, 0x61, 0x72, 0x20, 0x3d, 0x20, 0x78, 0x3b, 0x0,
+		}
+		scope := NewDocument()
+		scope.Append(C.Null("x"))
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: scope}
+		actual := C.CodeWithScope("foo", "var bar = x;", scope)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Int32", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x10,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value
+			0xe5, 0xff, 0xff, 0xff,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Int32("foo", -27)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Timestamp", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x11,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value
+			0x11, 0x0, 0x0, 0x0, 0x8, 0x0, 0x0, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Timestamp("foo", 8, 17)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Int64", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x12,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value
+			0xe5, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Int64("foo", -27)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("Decimal128", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x13,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+			// value
+			0xee, 0x02, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+			0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x3c, 0xb0,
+		}
+		d, _ := ast.ParseDecimal128("-7.50")
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.Decimal128("foo", d)
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("MinKey", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0xff,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.MinKey("foo")
+
+		requireElementsEqual(t, expected, actual)
+	})
+
+	t.Run("MaxKey", func(t *testing.T) {
+		buf := []byte{
+			// type
+			0x7f,
+			// key
+			0x66, 0x6f, 0x6f, 0x0,
+		}
+
+		expected := &Element{ReaderElement: ReaderElement{start: 0, value: 5, data: buf}, d: nil}
+		actual := C.MaxKey("foo")
+
+		requireElementsEqual(t, expected, actual)
+	})
+}
