cf693554f047b0d99d580173b794849f5aee6beb
Kris Brandow <kris@mongodb.com>
2018-01-22 15:13:16 -0500

    GODRIVER-185 Fix number of counter bytes for ObjectID
    Change-Id: I8596e0a09556890fa0bd3720fae2ce63ca228946

--- bson/objectid/objectid.go
+++ bson/objectid/objectid.go
@@ -22,14 +22,14 @@ import (
 type ObjectID [12]byte
 
 var objectIDCounter = readRandomUint32()
-var processUnique = readRandomUint32()
+var processUnique = processUniqueBytes()
 
 func New() ObjectID {
 	var b [12]byte
 
 	binary.BigEndian.PutUint32(b[0:4], uint32(time.Now().Unix()))
-	binary.BigEndian.PutUint32(b[4:8], processUnique)
-	binary.BigEndian.PutUint32(b[9:12], atomic.AddUint32(&objectIDCounter, 1))
+	copy(b[4:9], processUnique[:])
+	putUint24(b[9:12], atomic.AddUint32(&objectIDCounter, 1))
 
 	return b
 }
@@ -38,12 +38,28 @@ func (id *ObjectID) Hex() string {
 	return hex.EncodeToString(id[:])
 }
 
+func processUniqueBytes() [5]byte {
+	var b [5]byte
+	_, err := io.ReadFull(rand.Reader, b[:])
+	if err != nil {
+		panic(fmt.Errorf("cannot initialize objectid pacakge with crypto.rand.Reader: %v", err))
+	}
+
+	return b
+}
+
 func readRandomUint32() uint32 {
 	var b [4]byte
 	_, err := io.ReadFull(rand.Reader, b[:])
 	if err != nil {
-		panic(fmt.Errorf("cannot read random object id: %v", err))
+		panic(fmt.Errorf("cannot initialize objectid pacakge with crypto.rand.Reader: %v", err))
 	}
 
 	return (uint32(b[0]) << 0) | (uint32(b[1]) << 8) | (uint32(b[2]) << 16) | (uint32(b[3]) << 24)
+}
+
+func putUint24(b []byte, v uint32) {
+	b[0] = byte(v >> 16)
+	b[1] = byte(v >> 8)
+	b[2] = byte(v)
 }
